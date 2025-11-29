bcd1de842ed6c05899d32b418f779be9ff5da180
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2025-11-29 11:42:07 +0700

    Add 2 class for fare scheme

---
+++ CustomModels/Fare/FareScheme.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingSystemAPI.CustomModels.Fare
+{
+    public class FareScheme
+    {
+        public decimal Fare { get; set; }
+        public decimal UnitHour { get; set; }
+        public int MaxScheme { get; set; } //0 for unlimited
+    }
+}
---
+++ CustomModels/Fare/ParkingFare_Input.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingSystemAPI.CustomModels.Fare
+{
+    public class ParkingFare_Input
+    {
+        //public decimal Fare { get; set; }
+
+        //public byte UnitMasterId { get; set; }
+
+        public byte VehicleMasterId { get; set; }
+
+        public List<FareScheme> FareScheme { get; set; }
+
+        public byte IsActive { get; set; } = 0;
+    }
+}
